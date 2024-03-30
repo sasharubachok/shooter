08cb1baaf6d5080aa8700b8d8c84768e36b72c04
sasharubachok <145208639+sasharubachok@users.noreply.github.com>
2024-03-30 17:30:06 +0200

    Add files via upload

--- shooter_game.py
+++ shooter_game.py
@@ -1,118 +1,173 @@
 #Створи власний Шутер!
-
+import random
 from pygame import *
-import random 
-window = display.set_mode((700,500)) 
-bg = image.load("galaxy.jpg") 
-bg = transform.scale(bg, (700,500)) 
 
-game = True 
-clock = time.Clock()  
+window = display.set_mode((700, 500))
 
-mixer.init() 
+bg = image.load("galaxy.jpg")
+bg = transform.scale(bg, (700, 500))
+
+game = True
+clock = time.Clock()
+
+mixer.init()
 space = mixer.Sound("space.ogg")
-
 space.play()
 
-class Hero(sprite.Sprite): 
-    def __init__(self, x, y, width, height , speed, img_name="rocket.png"):  
+class Hero(sprite.Sprite):
+    def __init__ (self, x, y, width, height, speed, img_name="rocket.png"):
         super().__init__()
-        self.image = image.load(img_name) 
-        self.image = transform.scale(self. image, (width, height))
-        self.speed = speed 
+
+        self.image = image.load(img_name)
+        self.image = transform.scale(self.image, (width, height))
+        self.speed = speed
         self.rect = self.image.get_rect()
-        self.rect.x = x 
+        self.rect.x = x
         self.rect.y = y 
         
-    def reset(self): 
-        window.blit(self.image,(self.rect.x,self.rect.y)) 
+    def reset(self):
+        window.blit(self.image,(self.rect.x,self.rect.y))
 
-class Bullet(Hero): 
-    def move(self): 
-        self.rect.y -= self.speed 
-        if (self.rect.y < 0): 
-             self.kill()
 
-class Player(Hero): 
-    def move (self): 
-        keys = key.get_pressed() 
-        if keys [K_a] and self.rect.x > 5: 
-            self.rect.x -= self.speed 
+class Bullet(Hero):
+    def move(self):
+        self.rect.y -= self.speed
+        if (self.rect.y < 0):
+            self.kill()
 
-        if keys [K_d] and self.rect.x < 650: 
-            self.rect.x += self.speed 
 
-     
-    def fire(self): 
-        bullet = Bullet(self.rect.x, self.rect.y, 10,10,10, 'bullet.png') 
+class Player(Hero):
+    def move (self):
+        keys = key.get_pressed()
+        if keys [K_a] and self.rect.x > 5:
+            self.rect.x -= self.speed
+
+        if keys [K_d] and self.rect.x < 700:
+            self.rect.x += self.speed
+        
+    
+
+    def fire(self):
+        bullet = Bullet(self.rect.centerx, self.rect.y, 10, 10, 10, 'bullet.png')
         bullets.add(bullet)
-counter = 0
-killed = 0
 
 bullets = sprite.Group()
 
-
- 
-class Enemy (Hero): 
-    def move(self): 
-        self.rect.y += self.speed 
-        global counter  
+class Enemy (Hero):
+    def move(self):
+        self.rect.y += self.speed
+        global counter
         if self.rect.y > 560:
-            counter += 1 
-            self.rect.x = random.randint(50, 565) 
+            counter += 1
+            self.rect.x = random.randint(50, 565)
             self.speed = random.randint(1,4)
             self.rect.y = -100
+        
+class Asteroid(Hero):
+    def move(self):
+        self.rect.y += self.speed
+        self.rect.x += self.speed 
+        if self.rect.y > 560 or self.rect.x > 750:
+            self.rect.x = random.randint(-200,200) 
+            self.rect.y = -random.randint(50, 150)
 
-rocket = Player(350,400,50,85,5)
+rocket = Player(350,400,35,85,5)
 
-enemys = sprite.Group() 
-for i in range (5):
-    enemy1 = Enemy (350, -100, 100, 50,random.randint(3,5),"ufo.png") 
-    enemys.add(enemy1) 
-font.init() 
-lifes = 3
+enemys = sprite.Group()
+for i in range(5):
+    enemy1 = Enemy (random.randint(50, 565), -100, 100, 30, random.randint(1,4), "ufo.png")
+    enemys.add(enemy1)
+
+asteroids = sprite.Group() 
+for i in range(5): 
+    asteroid =Asteroid(random.randint(-200, 200), -random.randint(50, 150), 30, 30, random.randint(2,5), "asteroid.png") 
+    asteroids.add(asteroid) 
+
+font.init()
+
+font_win = font.Font(None, 60)
+
 font1 = font.Font(None, 40)
 finish = False
-while game:
-    
-    for e in event.get(): 
-        if e.type == QUIT: 
-            game = False  
-        if e.type == KEYDOWN: 
-            if e.key == K_SPACE: 
-                rocket.fire() 
-                
-    window.blit(bg,( 0,0)) 
-    if finish != True:
-        window.blit(font1.render(f"Лічильник:  {counter}",True, (255,255,255), (0,0,0)), (0,0)) 
-        window.blit(font1.render(f"Життя:  {lifes}",True, (255,255,255), (0,0,0)), (0,100)) 
-        window.blit(font1.render(f"Збиття:  {killed}",True, (255,255,255), (0,0,0)), (0,50)) 
-        
-        enemy1 = Enemy (350, -100, 150, 50,random.randint(3,5),"ufo.png")
-        for i in enemys:
-            i.reset() 
-            i.move() 
-        for b in bullets: 
-            b.reset()   
-            b.move()
-    
-        list_collaides = sprite.spritecollide(rocket,enemys,False) 
-        for colide in list_collaides:
-            if colide: 
-                lifes -= 1
-            for i in enemys: 
-                i.rect.y = -100
-                i.rect.x = random.randint(50, 565) 
+lifes = 3
+counter = 0
+killed = 0
 
-            list_colides =   sprite.groupcollide(bullets, enemys , True, False) 
-            for collide in list_colides: 
-                if collide:
-                    killed += 1
-                    enemy1 = Enemy (random.randint(50, 565), -100, 100, 300, random.randint(1,4), "ufo.png")
-                    enemys.add(enemy1)
-        enemy1.reset()      
-        enemy1.move()      
-        rocket.reset() 
+while game:
+    window.blit(bg, (0,0)) 
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+        if e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                rocket.fire() 
+            if e.key == K_r:
+                lifes = 3 
+                killed = 0 
+                counter = 0 
+                finish = False
+                for i in enemys:
+                    i.rect.y = -random.randint(50, 200)
+                    i.rect.x = random.randint(50, 565)
+    
+                for i in asteroids: 
+                    i.rect.y = -random.randint(50,200)
+                    i.rect.x = random.randint(-200,200)
+                    
+    if finish != True:
+        window.blit(font1.render(f"Лічилник: {counter}",True, (255,255,255), (0,0,0)), (0,0))
+        window.blit(font1.render(f"Життя: {lifes}",True, (255,255,255), (0,0,0)), (0,50))
+        window.blit(font1.render(f"Збито: {killed}",True, (255,255,255), (0,0,0)), (0,100))
+        for i in enemys:
+            i.reset()
+            i.move()
+
+        for b in bullets:
+            b.reset()
+            b.move()
+        for a in asteroids:
+            a.reset()
+            a.move()
+    
+        list_collides = sprite.spritecollide(rocket, enemys, False)
+        for collide in list_collides:
+            if collide:
+                lifes -= 1 
+
+                if lifes == 0:
+                    finish = True
+                for i in enemys:
+                    i.rect.y = -100
+                    i.rect.x = random.randint(50, 565)
+
+        list_collides = sprite.groupcollide(enemys, bullets, True, True)
+        for collide in list_collides:
+            if collide:
+                killed += 1
+                enemy1 = Enemy (random.randint(50, 565), -100, 100, 30, random.randint(1,4), "ufo.png")
+                enemys.add(enemy1)
+
+        list_collides = sprite.spritecollide(rocket, asteroids, True)
+        for collide in list_collides:
+            if collide:
+                lifes -= 1 
+                if lifes == 0:
+                    finish = True
+                
+                asteroid = Asteroid(random.randint(-200, 200), -random.randint(50 ,150), 30 , 30, random.randint(1,3), 'asteroid.png')
+                asteroids.add(asteroid)
+
+        rocket.reset()
         rocket.move()
-    clock.tick(60) 
-    display.update() 
+    if finish == True: 
+        if killed == 10:
+            window.blit(font_win.render("ТИ ВИГРАВ", True, (0,255,0)), (280,240)) 
+        if lifes == 0 or counter == 5:
+            window.blit(font_win.render("ТИ ПРОГРАВ", True, (255,0,0)), (250,240)) 
+        
+        window.blit(font_win.render("for restart please press R", True, (255,255,255)), (55,300))
+
+
+    clock.tick(60)
+    display.update()
